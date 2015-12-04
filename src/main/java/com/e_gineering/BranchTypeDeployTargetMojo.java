0397a74ea3797307f8b8c5fcd2cdaa5befe07f57
bvarner <bryan@varnernet.com>
2015-12-04 16:14:17 -0500

    * Do not directly invoke a specific version of maven-deploy-plugin, use the one configured by default if it's there. * Moved some common logging bits into AbstractGitBasedDeployMojo. * MasterPromotion now disables the local repository (oh this is not clean) when resolving the artifacts from test-releases * Cleaned up some imports.

--- src/main/java/com/e_gineering/BranchTypeDeployTargetMojo.java
+++ src/main/java/com/e_gineering/BranchTypeDeployTargetMojo.java
@@ -1,6 +1,5 @@
 package com.e_gineering;
 
-import org.apache.maven.model.DeploymentRepository;
 import org.apache.maven.model.DistributionManagement;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -20,39 +19,34 @@ public class BranchTypeDeployTargetMojo extends AbstractGitBasedDeployMojo {
 
         switch (type) {
             case MASTER: {
-                getLog().info("Building from master branch. Setting release artifact repository to: [" + releaseDeploymentRepository + "]");
+                getLog().info("Setting release artifact repository to: [" + releaseDeploymentRepository + "]");
                 project.setSnapshotArtifactRepository(null);
                 project.setReleaseArtifactRepository(getDeploymentRepository(releaseDeploymentRepository));
                 break;
             }
             case RELEASE: {
-                getLog().info("Building from release branch. Setting release artifact repository to: [" + testDeploymentRepository + "]");
+                getLog().info("Setting release artifact repository to: [" + testDeploymentRepository + "]");
                 project.setSnapshotArtifactRepository(null);
                 project.setReleaseArtifactRepository(getDeploymentRepository(testDeploymentRepository));
                 break;
             }
             case HOTFIX: {
-                getLog().info("Building from hotfix branch. Setting release artifact repository to: [" + testDeploymentRepository + "]");
+                getLog().info("Setting release artifact repository to: [" + testDeploymentRepository + "]");
                 project.setSnapshotArtifactRepository(null);
                 project.setReleaseArtifactRepository(getDeploymentRepository(testDeploymentRepository));
                 break;
             }
-            case BUGFIX: {
-                getLog().info("Building from bugfix branch. Un-Setting artifact repositories");
-                project.setSnapshotArtifactRepository(null);
-                project.setReleaseArtifactRepository(null);
-                break;
-            }
             case DEVELOPMENT: {
-                getLog().info("Building from development branch. Setting snapshot artifact repository to: [" + snapshotDeploymentRepository + "]");
+                getLog().info("Setting snapshot artifact repository to: [" + snapshotDeploymentRepository + "]");
                 project.setSnapshotArtifactRepository(getDeploymentRepository(snapshotDeploymentRepository));
                 project.setReleaseArtifactRepository(null);
                 break;
             }
-            case OTHER: {
-                getLog().info("Building from arbitrary branch [" + gitBranch + "]. Un-setting artifact repositories.");
+            default: {
+                getLog().info("Un-Setting artifact repositories");
                 project.setSnapshotArtifactRepository(null);
                 project.setReleaseArtifactRepository(null);
+                break;
             }
         }
     }
--- src/main/java/com/e_gineering/PromoteMasterMojo.java
+++ src/main/java/com/e_gineering/PromoteMasterMojo.java
@@ -13,21 +13,23 @@ import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.impl.ArtifactResolver;
+import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.ArtifactResult;
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
-
 /**
  * Set the target repository for deployment based upon the GIT_BRANCH being built.
  */
-@Mojo(name = "promote-master", defaultPhase = LifecyclePhase.DEPLOY)
+@Mojo(name = "promote-master", defaultPhase = LifecyclePhase.INSTALL)
 public class PromoteMasterMojo extends AbstractGitBasedDeployMojo {
 
     @Component
@@ -50,7 +52,7 @@ public class PromoteMasterMojo extends AbstractGitBasedDeployMojo {
     protected void execute(final GitBranchType type) throws MojoExecutionException, MojoFailureException {
         switch (type) {
             case MASTER: {
-                getLog().info("Building from master branch. Attempting to resolve tested artifacts from [" + testDeploymentRepository + "]");
+                getLog().info("Attaching existing artifacts from testDeploymentRepository [" + testDeploymentRepository + "]");
 
                 List<RemoteRepository> remoteRepositories = Arrays.asList(getRepository(testDeploymentRepository));
 
@@ -60,6 +62,27 @@ public class PromoteMasterMojo extends AbstractGitBasedDeployMojo {
 
                 // Build up a set of ArtifactRequests, for the pom, the current packaging layout, the -sources.jar and the -javadoc.jar and the
                 List<ArtifactRequest> requiredArtifacts = new ArrayList<ArtifactRequest>();
+
+
+                // Keep track of the original base directory.
+                getLog().info("Disabling local repository @ " + session.getLocalRepository().getBasedir());
+                Field localBaseDir = null;
+                File originalBaseDir = session.getLocalRepositoryManager().getRepository().getBasedir();
+
+                // Disable the local repository.
+                try {
+                    localBaseDir = LocalRepository.class.getDeclaredField("basedir");
+                    localBaseDir.setAccessible(true);
+
+                    // Generate a new temp directory.
+                    File tempRepo = Files.createTempDirectory("gitflow-helper-maven-plugin-repo").toFile();
+                    tempRepo.deleteOnExit();
+
+                    getLog().info("Using temporary local repository @ " + tempRepo.getAbsolutePath());
+                    localBaseDir.set(session.getLocalRepositoryManager().getRepository(), tempRepo);
+                } catch (Exception ex) {
+                    getLog().warn("Failed to disable local repository path.", ex);
+                }
 
                 // This is required!
                 requiredArtifacts.add(new ArtifactRequest(new DefaultArtifact(project.getGroupId(), project.getArtifactId(), project.getPackaging(), project.getVersion()), remoteRepositories, null));
@@ -94,26 +117,16 @@ public class PromoteMasterMojo extends AbstractGitBasedDeployMojo {
                     }
                 }
 
-                // Invoke the Deploy task to attach the resolved (and attached to this build) artifacts.
-                executeMojo(
-                        plugin(
-                                groupId("org.apache.maven.plugins"),
-                                artifactId("maven-deploy-plugin"),
-                                version("2.8.2")
-                        ),
-                        goal("deploy"),
-                        configuration(),
-                        executionEnvironment(
-                                project,
-                                mavenSession,
-                                pluginManager
-                        )
-                );
+                // Restore the local repository.
+                try {
+                    localBaseDir.set(session.getLocalRepositoryManager().getRepository(), originalBaseDir);
+                    localBaseDir.setAccessible(false);
+                } catch (Exception ex) {
+                    getLog().warn("Failed to restore original local repository path.", ex);
+                }
+
 
                 break;
-            }
-            default: {
-                getLog().debug("Building from non-master branch [" + gitBranch + "]. Build will continue as configured.");
             }
         }
     }
