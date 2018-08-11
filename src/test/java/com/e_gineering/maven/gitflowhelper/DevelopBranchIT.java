c888df7d961ba332062bccf7722644f98e047471
bvarner <bryan@varnernet.com>
2018-08-11 10:38:05 -0400

    Updated with assumption.

--- src/test/java/com/e_gineering/maven/gitflowhelper/DevelopBranchIT.java
+++ src/test/java/com/e_gineering/maven/gitflowhelper/DevelopBranchIT.java
@@ -1,8 +1,12 @@
 package com.e_gineering.maven.gitflowhelper;
 
 import org.apache.maven.it.Verifier;
+import org.junit.Assume;
+import org.junit.internal.AssumptionViolatedException;
 
 public class DevelopBranchIT extends AbstractIntegrationTest {
+	private static boolean deployPassed = false;
+
 	/**
 	 * Non-snapshot versions on the develop branch should fail.
 	 */
@@ -15,5 +19,41 @@ public class DevelopBranchIT extends AbstractIntegrationTest {
 			verifier.verifyTextInLog("The current git branch: [origin/develop] is detected as a SNAPSHOT-type branch, and expects a maven project version ending with -SNAPSHOT. The maven project version found was: [1.0.0]");
 		}
 		verifier.resetStreams();
+	}
+
+	/**.
+	 * Snapshot versions on the develop branch should pass
+	 * @throws Exception
+	 */
+	public void testSnapshotDeploySuccess() throws Exception {
+		Verifier verifier = createVerifier("/project-stub", "origin/develop", "1.0.0-SNAPSHOT");
+
+		verifier.setAutoclean(true);
+		verifier.executeGoal("deploy");
+
+		verifier.verifyErrorFreeLog();
+
+		verifier.resetStreams();
+		deployPassed = true;
+	}
+
+	/**
+	 * Attaching existing artifacts from the develop branch should pass.
+	 *
+	 * @throws Exception
+	 */
+	public void testAttachExistingArtifacts() throws Exception {
+		// If it didn't pass...
+		try {
+			Assume.assumeTrue(deployPassed);
+		} catch (AssumptionViolatedException ave) {
+			Assume.assumeNoException(ave);
+		}
+
+		Verifier verifier = createVerifier("/project-stub", "origin/develop", "1.0.0-SNAPSHOT");
+
+		verifier.executeGoal("gitflow-helper:attach-deployed");
+
+		verifier.verifyErrorFreeLog();
 	}
 }
