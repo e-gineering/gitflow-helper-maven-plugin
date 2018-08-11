ea3c1d680cd7e95a4a988ba6c05dd8a086ccef0d
bvarner <bryan@varnernet.com>
2018-08-11 10:03:24 -0400

    Finished configuring the extension to hook in the lifecycle goals.

--- src/test/java/com/e_gineering/maven/gitflowhelper/DevelopBranchIT.java
+++ src/test/java/com/e_gineering/maven/gitflowhelper/DevelopBranchIT.java
@@ -1,4 +1,19 @@
 package com.e_gineering.maven.gitflowhelper;
 
+import org.apache.maven.it.Verifier;
+
 public class DevelopBranchIT extends AbstractIntegrationTest {
+	/**
+	 * Non-snapshot versions on the develop branch should fail.
+	 */
+	public void testNonSnapshotDeployFails() throws Exception {
+		Verifier verifier = createVerifier("/project-stub", "origin/develop", "1.0.0");
+		verifier.setAutoclean(true);
+		try {
+			verifier.executeGoal("deploy");
+		} catch (Exception ex) {
+			verifier.verifyTextInLog("The current git branch: [origin/develop] is detected as a SNAPSHOT-type branch, and expects a maven project version ending with -SNAPSHOT. The maven project version found was: [1.0.0]");
+		}
+		verifier.resetStreams();
+	}
 }
