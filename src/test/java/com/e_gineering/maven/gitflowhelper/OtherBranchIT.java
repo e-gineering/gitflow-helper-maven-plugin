cbcf0bdac572064da1a5cc97517aab5b603f1748
bvarner <bryan@varnernet.com>
2018-08-15 16:29:50 -0400

    I forgot to finalize.

--- src/test/java/com/e_gineering/maven/gitflowhelper/OtherBranchIT.java
+++ src/test/java/com/e_gineering/maven/gitflowhelper/OtherBranchIT.java
@@ -10,52 +10,65 @@ public class OtherBranchIT extends AbstractIntegrationTest {
 	@Test
 	public void featureSnapshotSemVer() throws Exception {
 		Verifier verifier = createVerifier("/project-stub", "origin/feature/poc/my-feature-branch", "5.0.0-SNAPSHOT");
-		verifier.executeGoal("deploy");
+		try {
+			verifier.executeGoal("deploy");
 
-		verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-my-feature-branch-SNAPSHOT");
-		verifier.verifyErrorFreeLog();
-		verifier.resetStreams();
+			verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-my-feature-branch-SNAPSHOT");
+			verifier.verifyErrorFreeLog();
+		} finally {
+			verifier.resetStreams();
+		}
 	}
 
 	@Test
 	public void featureSemVer() throws Exception {
 		Verifier verifier = createVerifier("/project-stub", "origin/feature/poc/my-feature-branch.with.other.identifiers", "5.0.1");
-		verifier.executeGoal("deploy");
+		try {
+			verifier.executeGoal("deploy");
 
-		verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-my-feature-branch.with.other.identifiers-SNAPSHOT");
-		verifier.verifyErrorFreeLog();
-		verifier.resetStreams();
+			verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-my-feature-branch.with.other.identifiers-SNAPSHOT");
+			verifier.verifyErrorFreeLog();
+		} finally {
+			verifier.resetStreams();
+		}
 	}
 
 	@Test
 	public void noOtherDeployMatch() throws Exception {
 		Verifier verifier = createVerifier("/project-stub", "origin/feature/my-feature-branch", "5.0.1-SNAPSHOT");
-		verifier.executeGoal("deploy");
+		try {
+			verifier.executeGoal("deploy");
 
-		verifier.verifyTextInLog("Un-Setting artifact repositories.");
-		verifier.verifyErrorFreeLog();
-		verifier.resetStreams();
+			verifier.verifyTextInLog("Un-Setting artifact repositories.");
+			verifier.verifyErrorFreeLog();
+		} finally {
+			verifier.resetStreams();
+		}
 	}
 
 	@Test
 	public void automagicVersionDependenciesResolve() throws Exception {
 		// Create a -SNAPSHOT of the project-stub.
 		Verifier verifier = createVerifier("/project-stub", "origin/feature/poc/long-running", "2.0.0");
+		try {
+			verifier.executeGoal("deploy");
 
-		verifier.executeGoal("deploy");
-
-		verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-long-running-SNAPSHOT");
-		verifier.verifyErrorFreeLog();
-		verifier.resetStreams();
+			verifier.verifyTextInLog("Artifact versions updated with semVer build metadata: +origin-feature-poc-long-running-SNAPSHOT");
+			verifier.verifyErrorFreeLog();
+		} finally {
+			verifier.resetStreams();
+		}
 
 		// Create a -SNAPSHOT of the project-alt1-stub that depends upon the other project's automagic version.
 		verifier = createVerifier("/project-alt1-stub", "origin/feature/poc/long-running", "2.0.0");
+		try {
+			verifier.getCliOptions().add("-Ddependency.stub.version=2.0.0+origin-feature-poc-long-running-SNAPSHOT");
+			verifier.getCliOptions().add("-Dplugin.stub.version=2.0.0+origin-feature-poc-long-running-SNAPSHOT");
 
-		verifier.getCliOptions().add("-Ddependency.stub.version=2.0.0+origin-feature-poc-long-running-SNAPSHOT");
-		verifier.getCliOptions().add("-Dplugin.stub.version=2.0.0+origin-feature-poc-long-running-SNAPSHOT");
-
-		verifier.executeGoal("deploy");
-		verifier.verifyErrorFreeLog();
-		verifier.resetStreams();
+			verifier.executeGoal("deploy");
+			verifier.verifyErrorFreeLog();
+		} finally {
+			verifier.resetStreams();
+		}
 	}
 }
