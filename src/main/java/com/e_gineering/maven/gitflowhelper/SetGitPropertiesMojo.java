73ebde3b964045a00b93e7ce202bc4c66a6fe104
Hierlmeier, Richard <richard.hierlmeier@de-gmbh.com>
2023-04-14 15:49:34 +0200

    SetGitPropertiesMojo introduced

---
+++ src/main/java/com/e_gineering/maven/gitflowhelper/SetGitPropertiesMojo.java
@@ -0,0 +1,59 @@
+package com.e_gineering.maven.gitflowhelper;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.codehaus.plexus.util.StringUtils;
+
+/**
+ * Stores the branch type and the git branch in Maven properties
+ */
+@Mojo(name = "set-git-info-properties", defaultPhase = LifecyclePhase.VALIDATE, threadSafe = true)
+public class SetGitPropertiesMojo extends AbstractGitflowBranchMojo
+{
+    /**
+     * Defines the name of the property where the branch type is stored
+     */
+    @Parameter(property = "branchTypeProperty", defaultValue = "branchType")
+    private String branchTypeProperty = "branchType";
+
+    /**
+     * Defines the name of the property where the Git branch name is stored
+     */
+    @Parameter(property = "branchNameProperty", defaultValue = "gitBranchName")
+    private String branchNameProperty = "branchName";
+
+    /**
+     * Der branchNamePropertyMapper allows to store the Git branch name
+     * into additional properties.
+     * The branchName can be mapped by a java class, or an JSR223 scripting language
+     */
+    @Parameter(property = "branchNamePropertyMappers")
+    private PropertyMapper[] branchNamePropertyMappers;
+
+    @Override
+    protected void execute(final GitBranchInfo gitBranchInfo) throws MojoExecutionException, MojoFailureException
+    {
+        if(!StringUtils.isBlank(branchTypeProperty)) {
+            project.getProperties().setProperty(branchTypeProperty, gitBranchInfo.getType().name());
+        }
+        if(!StringUtils.isBlank(branchNameProperty)) {
+            project.getProperties().setProperty(branchNameProperty, gitBranchInfo.getName());
+        }
+
+        if(branchNamePropertyMappers != null) {
+            for(PropertyMapper pm : branchNamePropertyMappers)
+            {
+                String mappedValue = pm.map(gitBranchInfo.getName());
+                getLog().info("Mapper [" + pm.getId() + "] mapped git branch name [" + gitBranchInfo.getName() + "] to [" + mappedValue + "]");
+                if(StringUtils.isBlank(mappedValue)) {
+                    getLog().warn("Mapper " + pm.getId() + " Git branch name did not provide a value, ignoring it");
+                    continue;
+                }
+                project.getProperties().setProperty(pm.getPropertyName(), mappedValue);
+            }
+        }
+    }
+}
